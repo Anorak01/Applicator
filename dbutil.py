ba186d065be8eca8557cecec0fed21d0450d07f2
Anorak01 <adamfri007@gmail.com>
2024-07-18 21:56:01 +0200

    Fixed up some pyright errors

--- dbutil.py
+++ dbutil.py
@@ -7,21 +7,24 @@ from action import Action, ActionInteraction
 
 
 class MessageDB():
-    def add_application_msg(msg_id: str, author_id: str, guild_id: str, app_name: str) -> None:
+    @classmethod
+    def add_application_msg(cls, msg_id: str, author_id: str, guild_id: str, app_name: str) -> None:
         data = (msg_id, author_id, guild_id, app_name)
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("INSERT INTO app_msg_db VALUES (?, ?, ?, ?)", data)
         con.commit()
 
-    def get_application_msg(msg_id: str) -> tuple[str, str]:
+    @classmethod
+    def get_application_msg(cls, msg_id: str) -> tuple[str, str, str]:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute(f"SELECT user_id, guild_id, app_name FROM app_msg_db WHERE msg_id={msg_id}")
         user_id, guild_id, app_name = cur.fetchone()
         return user_id, guild_id, app_name
 
-    def remove_application_msg(msg_id: str) -> None:
+    @classmethod
+    def remove_application_msg(cls, msg_id: str) -> None:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute(f"DELETE FROM app_msg_db WHERE msg_id={msg_id}")
@@ -29,28 +32,32 @@ class MessageDB():
 
 
 class StartButtonDB():
-    def add_start_msg(msg_id: str, app_name: str, guild_id: str) -> None:
+    @classmethod
+    def add_start_msg(cls, msg_id: str, app_name: str, guild_id: str) -> None:
         data = (msg_id, app_name, guild_id)
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("INSERT INTO app_start_db VALUES (?, ?, ?)", data)
         con.commit()
 
-    def get_start_msg(msg_id: str) -> tuple[str, str]:
+    @classmethod
+    def get_start_msg(cls, msg_id: str) -> tuple[str, str]:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT app_name, guild_id FROM app_start_db WHERE msg_id=?", (str(msg_id), ))
         app_name, guild_id = cur.fetchone()
         return app_name, guild_id
 
-    def remove_start_msg(msg_id: str) -> None:
+    @classmethod
+    def remove_start_msg(cls, msg_id: str) -> None:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute(f"DELETE FROM app_start_db WHERE msg_id={msg_id}")
         con.commit()
 
 class GuildAppDB():
-    def create_guild(guild_id: str, guild_name: str) -> None:
+    @classmethod
+    def create_guild(cls, guild_id: str, guild_name: str) -> None:
         applications = {}
         application_blob = pickle.dumps(applications)
         data = guild_id, guild_name, application_blob
@@ -59,13 +66,15 @@ class GuildAppDB():
         cur.execute("INSERT INTO app_guildapp_db VALUES (?, ?, ?)", data)
         con.commit()
 
-    def remove_guild(guild_id: str) -> None:
+    @classmethod
+    def remove_guild(cls, guild_id: str) -> None:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("DELETE FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
         con.commit()
 
-    def get_all_guilds() -> list:
+    @classmethod
+    def get_all_guilds(cls) -> list:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT guild_id FROM app_guildapp_db")
@@ -73,7 +82,8 @@ class GuildAppDB():
         guilds = [guild[0] for guild in guilds]
         return guilds
 
-    def add_application_entry(guild_id: str, application_name: str) -> str:
+    @classmethod
+    def add_application_entry(cls, guild_id: str, application_name: str) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -93,7 +103,8 @@ class GuildAppDB():
         else:
             return "error on add application entry: application exists"
 
-    def remove_application_entry(guild_id: str, application_name: str) -> str:
+    @classmethod
+    def remove_application_entry(cls, guild_id: str, application_name: str) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -108,7 +119,8 @@ class GuildAppDB():
         else:
             return "error on remove application entry: application not found"
 
-    def get_application_entry(guild_id: str, application_name: str):
+    @classmethod
+    def get_application_entry(cls, guild_id: str, application_name: str):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -117,7 +129,8 @@ class GuildAppDB():
         if application_name in applications.keys():
             return applications[application_name]
 
-    def get_applications(guild_id: str):
+    @classmethod
+    def get_applications(cls, guild_id: str):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -125,7 +138,8 @@ class GuildAppDB():
         applications = pickle.loads(application_blob[0])
         return list(applications.keys())
 
-    def set_response_channel(guild_id: str, application_name: str, channel_id: str) -> str:
+    @classmethod
+    def set_response_channel(cls, guild_id: str, application_name: str, channel_id: str) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -140,7 +154,8 @@ class GuildAppDB():
         else:
             return "error on set response channel: application not found"
 
-    def get_response_channel(guild_id: str, application_name: str) -> str:
+    @classmethod
+    def get_response_channel(cls, guild_id: str, application_name: str) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -149,7 +164,8 @@ class GuildAppDB():
         if application_name in applications.keys():
             return applications[application_name]["resp_channel"]
 
-    def add_question(guild_id: str, application_name: str, question: str) -> str:
+    @classmethod
+    def add_question(cls, guild_id: str, application_name: str, question: str) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -165,7 +181,8 @@ class GuildAppDB():
         else:
             return "error on add question: application not found"
 
-    def get_questions(guild_id: str, application_name: str):
+    @classmethod
+    def get_questions(cls, guild_id: str, application_name: str):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -177,7 +194,8 @@ class GuildAppDB():
         else:
             return "error on get questions: application not found", ""
 
-    def edit_question(guild_id: str, application_name: str, question_index: int, new_question: str):
+    @classmethod
+    def edit_question(cls, guild_id: str, application_name: str, question_index: int, new_question: str):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -197,7 +215,8 @@ class GuildAppDB():
         else:
             return "error on edit question: application not found"
 
-    def move_question(guild_id: str, application_name: str, init_que_index: int, fin_que_index: int):
+    @classmethod
+    def move_question(cls, guild_id: str, application_name: str, init_que_index: int, fin_que_index: int):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -224,7 +243,8 @@ class GuildAppDB():
         else:
             return "error on move question: application not found"
 
-    def remove_question(guild_id: str, application_name: str, question_index: int):
+    @classmethod
+    def remove_question(cls, guild_id: str, application_name: str, question_index: int):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -244,10 +264,8 @@ class GuildAppDB():
         else:
             return "error on remove question: application not found"
 
-
-
-
-    def add_action(guild_id: str, application_name: str, action: dict) -> str:
+    @classmethod
+    def add_action(cls, guild_id: str, application_name: str, action: dict) -> str:
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -262,7 +280,8 @@ class GuildAppDB():
         else:
             return "error on add action: application not found"
 
-    def get_actions(guild_id: str, application_name: str, action_type: ActionInteraction):
+    @classmethod
+    def get_actions(cls, guild_id: str, application_name: str, action_type: ActionInteraction):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -278,7 +297,8 @@ class GuildAppDB():
         else:
             return "error on get actions: application not found"
 
-    def remove_action(guild_id: str, application_name: str, action_type: ActionInteraction, action_index: int):
+    @classmethod
+    def remove_action(cls, guild_id: str, application_name: str, action_type: ActionInteraction, action_index: int):
         con = sqlite3.connect("applications.db")
         cur = con.cursor()
         cur.execute("SELECT applications_blob FROM app_guildapp_db WHERE guild_id=(?)", (guild_id, ))
@@ -293,7 +313,7 @@ class GuildAppDB():
                     actedit.append(i)
                 else:
                     actnoedit.append(i)
-            actedit
+
             if action_index <= len(actedit):
                 actedit.pop(action_index-1)
 
@@ -309,4 +329,3 @@ class GuildAppDB():
                 return "error on remove action: action index not found"
         else:
             return "error on remove action: application not found"
-
