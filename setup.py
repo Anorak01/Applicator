5a1a32b26c944fb9782b52d4f1945c90ff3ce42e
Anorak01 <adamfri007@gmail.com>
2024-07-19 17:40:09 +0200

    Updated columns in setup.py
    Added two columns to the app_guildapp_db table for editor and reviewer
    role ids

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import sqlite3
 con = sqlite3.connect("applications.db")
 cur = con.cursor()
 cur.execute("CREATE TABLE app_msg_db(msg_id, user_id, guild_id, app_name)")
-cur.execute("CREATE TABLE app_guildapp_db(guild_id, guild_name, applications_blob)")
+cur.execute("CREATE TABLE app_guildapp_db(guild_id, guild_name, applications_blob, editor_role_id, reviewer_role_id)")
 cur.execute("CREATE TABLE app_start_db(msg_id, app_name, guild_id)")
 con.commit()
